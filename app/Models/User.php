fb56a38dbed81183f74743ce4bee19beb30f5cb0
hanhongda <hansir1993@outlook.com>
2017-12-03 07:43:46 +0000

    Add user following

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 use App\Notifications\ResetPassword;
+use Auth;
 
 class User extends Authenticatable
 {
@@ -64,11 +65,54 @@ class User extends Authenticatable
     }
 
     /**
-     * 取出个人发布的微博按照创建时间排序
+     * 微博动态流
      */
     public function feed()
     {
-        return $this->statuses()
-                    ->orderBy('created_at','desc');
+      $user_ids = Auth::user()->followings->pluck('id')->toArray();
+      array_push($user_ids, Auth::user()->id);
+      return Status::whereIn('user_id', $user_ids)
+                            ->with('user')
+                            ->orderBy('created_at', 'desc');
+    }
+
+    /**
+     * 粉丝关系模型--多对多
+     */
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    /**
+     * 关注和取消关注
+     */
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids,false);
+    }
+
+    public function unfollow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    /**
+     * 判断是否已关注
+     */
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
     }
 }
